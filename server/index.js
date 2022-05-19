6b0fceb79278bfd3a15157a0211849a5bbf7d666
Mukiibi-G-J <mukiibijosephgilbert865@gmail.com>
2022-05-19 14:53:44 +0300

    connecting to a monogdb dattabase

--- server/index.js
+++ server/index.js
@@ -8,4 +8,22 @@ const app = express();
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
+const CONNECTION_URL =
+  'mongodb+srv://admin:mukiibi@cluster0.fi0gk.mongodb.net/?retryWrites=true&w=majority';
+
 app.use(cors);
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    //? this  makes limits the errors
+    useNewUrlParser: true,
+    //? this  makes limits the errors
+    useUnifiedTopology: true,
+  })
+  //? the listen function takes in port and callback
+  .then(() => app.listen(PORT, () => console.log(`Server running : ${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+//? this  makes limits the errors
+mongoose.set('useFindAndModify', false);
